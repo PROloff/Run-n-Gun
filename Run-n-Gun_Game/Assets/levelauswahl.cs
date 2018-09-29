dcbdbde04cb30b4035b9dfba76925204798a667c
ItzLenny <sajiqu@googlemail.com>
2018-09-29 21:11:21 +0200

    alpha

--- Run-n-Gun_Game/Assets/levelauswahl.cs
+++ Run-n-Gun_Game/Assets/levelauswahl.cs
@@ -12,9 +12,17 @@ public class levelauswahl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Menü"))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("levelauswahlw");
+		if (Input.GetButtonDown("A"))
+        {	Debug.Log("test");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("lvl_2");
         }
+		if (Input.GetButtonDown("X"))
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("lvl_1");
+		}
+		if (Input.GetButtonDown("B"))
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("lvl_3");
+		}
 	}
 }
