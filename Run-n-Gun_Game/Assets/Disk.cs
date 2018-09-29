0b9209f1c63adee8a9b16ae6762ab589d84d1c0f
PROloff <proloff1@lspb.de>
2018-09-29 10:12:28 +0200

    repair

--- Run-n-Gun_Game/Assets/Disk.cs
+++ Run-n-Gun_Game/Assets/Disk.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Disk : MonoBehaviour {
 
 	// Use this for initialization
+	public int m_PlayerNumber = 1;  
+	private string m_FireButton;
+	private float m_ChargeSpeed = 15f;
 	void Start () {
-		
+		m_FireButton = "Fire" + m_PlayerNumber;
+
+        // The rate that the launch force charges up is the range of possible forces by the max charge time.
+    
 	}
 	
 	// Update is called once per frame
