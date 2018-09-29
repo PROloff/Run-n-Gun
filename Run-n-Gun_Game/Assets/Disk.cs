ce47cbf645365c3606c64c11ffafcd09082de97f
PROloff <proloff1@lspb.de>
2018-09-29 10:50:25 +0200

    disk shoot

--- Run-n-Gun_Game/Assets/Disk.cs
+++ Run-n-Gun_Game/Assets/Disk.cs
@@ -8,6 +8,9 @@ public class Disk : MonoBehaviour {
 	public int m_PlayerNumber = 1;  
 	private string m_FireButton;
 	private float m_ChargeSpeed = 15f;
+	private bool m_Fired;
+	public Transform m_FireTransform;
+	public Rigidbody m_Shell;
 	void Start () {
 		m_FireButton = "Fire" + m_PlayerNumber;
 
@@ -17,6 +20,30 @@ public class Disk : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (!m_Fired && Input.GetButtonDown (m_FireButton))
+		{
+			Fire();
+		}
 		
 	}
+	 private void Fire ()
+    {
+        // Set the fired flag so only Fire is only called once.
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_ChargeSpeed * m_FireTransform.forward; ;
+
+        // Change the clip to the firing clip and play it.
+
+       // m_ShootingAudio.clip = m_FireClip;
+       // m_ShootingAudio.Play ();
+
+        // Reset the launch force.  This is a precaution in case of missing button events.
+       
+    }
 }
