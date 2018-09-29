22f599fa46f5bdf7e2a9ae252e3e7402318d3bef
unknown <miguel.driesner@gmail.com>
2018-09-29 16:23:03 +0200

    sex

---
+++ Run-n-Gun_Game/Assets/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown.Equals(2))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Paul_Szene");
+        }
+	}
+}
