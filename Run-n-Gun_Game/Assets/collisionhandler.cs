bd0632449cb54dadf2c2717f4852c0497665c6b6
PROloff <proloff1@lspb.de>
2018-09-29 14:28:09 +0200

    disk destroy

---
+++ Run-n-Gun_Game/Assets/collisionhandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionhandler : MonoBehaviour {
+
+	// Use this for initialization
+	public Player player;
+	void Start () {
+		player = this.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision col)
+    {
+		
+        if(col.gameObject.name == "Player" || col.gameObject.name == "Disc (Clone)" || col.gameObject.name == "Walltop" || col.gameObject.name == "Wallleft" || col.gameObject.name == "Walldown" || col.gameObject.name == "Wallright")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
