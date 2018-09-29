54d7e12376b4efe29687052ce76b34bc497224e3
unknown <chris.artmann@outlook.de>
2018-09-29 11:19:38 +0200

    nexttry2

---
+++ Run-n-Gun_Game/Assets/sound/SoundScripts/Sound_OnEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_OnEvent : MonoBehaviour {
+
+    public AudioSource Sound1;
+
+    private void Awake()
+    {
+        Sound1 = GetComponent();
+    }
+
+    // Use this for initialization
+    void Start () {
+		if()
+        {
+            Sound1.Play();
+        } //if
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
