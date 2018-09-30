0f18c3024034889321f21ef2b6f1b24bc9d85702
ItzLenny <sajiqu@googlemail.com>
2018-09-30 12:28:19 +0200

    FINALGAME

--- Run-n-Gun_Game/Assets/collisionhandler.cs
+++ Run-n-Gun_Game/Assets/collisionhandler.cs
@@ -35,7 +35,7 @@ public class collisionhandler : MonoBehaviour
             exist = false;
         }
 
-        if (collisions != 2)
+        if (collisions != 3)
         {
             collisions++;
         }
